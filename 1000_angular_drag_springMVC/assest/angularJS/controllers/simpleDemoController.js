b6b5e7b74520f073db77887d0a35e6fdc1da8ac5
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-03-25 21:09:48 +0800

    完成拖拉可發送部門編號查詢

--- 1000_angular_drag_springMVC/assest/angularJS/controllers/simpleDemoController.js
+++ 1000_angular_drag_springMVC/assest/angularJS/controllers/simpleDemoController.js
@@ -1,4 +1,4 @@
-myApp.controller("SimpleDemoController",  ['$scope', 'DeptService', function ($scope, deptService) {
+myApp.controller("SimpleDemoController", ['$scope', 'DeptService', function ($scope, deptService) {
     $scope.models = {
         selected: null,
         lists: {
@@ -16,15 +16,35 @@ myApp.controller("SimpleDemoController",  ['$scope', 'DeptService', function ($s
 
     // Model to JSON for demo purpose
     $scope.$watch('models', function (model) {
-        $scope.modelAsJson = angular.toJson(model, true /*format JSON*/ );
+        $scope.modelAsJson = angular.toJson(model, true /*format JSON*/ ); // angular.toJson() 底層 call JSON.Stringfy()
+        
+        if (model.lists['submitData'].length == 0) {
+            $scope.hasPrepared = false;
+        } else {
+            $scope.hasPrepared = true;
+        }
+
     }, true /*Deep Watch*/ );
 
+    $scope.doSubmit = function () {
+        console.log('=== doSubmit ===');
+        // console.log('=== $scope.model ===', $scope.model);// 一直是 undefined
+        // console.log('=== $scope.modelAsJson ===', $scope.modelAsJson);
+        let modelasJsonObj = angular.fromJson($scope.modelAsJson);
+        let deptVOSubmitArray = modelasJsonObj.lists['submitData'];
+        // console.log("=== $scope.modelAsJson.lists['submitData'] ===", deptVOSubmitArray);
 
-    $scope.doSubmit = function(){
-        alert('=== doSubmit ===');
-        deptService.getDeptAndBelowEmps(10).then(function (cbData) {
-            console.info('cbData',cbData);
+        let deptIdArray = deptVOSubmitArray.map((item) => { // 利用map方法改變陣列並回傳
+            return item['DeptNo'];
         });
+        // console.log('deptIdArray >>> ' , deptIdArray);
+
+        let isEager = true;
+        deptService.getDeptAndBelowEmps(isEager, deptIdArray)
+            .then(function (cbData) {
+                console.info('cbData', cbData);
+            });
+
     };
 
 }]);
--- 1000_angular_drag_springMVC/assest/angularJS/services/deptService.js
+++ 1000_angular_drag_springMVC/assest/angularJS/services/deptService.js
@@ -1,13 +1,12 @@
 myApp.service('DeptService', ['$http', '$q', function ($http, $q) {
 
-    this.getDeptAndBelowEmps = function(_deptNo) {
+    this.getDeptAndBelowEmps = function( _isEager , _deptNOs ) {
         let deferred = $q.defer();
-        let params = { "_isEager" : false , "deptNoArray" : [ 10, 20 ] };
+        let params = { "deptNoArray" : _deptNOs };
         console.info('angular.toJson(params) >>> ' , angular.toJson(params));
         $http({
             method: 'POST',
-            url: `http://127.0.0.1:8080/Angular1_JPA_test/spring/QueryController/query/getDeptByIdList`,
-            // url: `http://127.0.0.1:8080/Angular1_JPA_test/spring/testController/ggg/fuck`,
+            url: `http://127.0.0.1:8080/Angular1_JPA_test/spring/QueryController/query/getDeptByIdList/isEager=${_isEager}/`,
             data: angular.toJson(params),
             headers: {
                 "Content-Type": "application/json; charset=utf-8;"
@@ -15,7 +14,6 @@ myApp.service('DeptService', ['$http', '$q', function ($http, $q) {
         }).then(function (response) {
             // console.log('success response', response);
             deferred.resolve(response.data);
-            // console.log('success busDataArray', busDataArray);
         }, function (response) {
             console.log('error response', response);
         }); 
