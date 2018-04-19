1919fc3b97072c35f2f72b60bda1424054663041
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-04-19 21:53:31 +0800

    angular document ready

--- 004_ngBind/index.js
+++ 004_ngBind/index.js
@@ -1,9 +1,9 @@
+var myApp = angular.module('myApp', []);
 
-var firstController = function($scope){
-    
+myApp.controller('firstController', ['$scope', '$rootScope', function ($$scope, $$rootScope) {
     // $scope物件 => 作用域
     // 宣告model
 
-    $scope.empName = 'Roger';
-    $scope.empSalary = 22000;
-}
+    $$scope.empName = 'Roger';
+    $$scope.empSalary = 22000;
+}]);
